b3c80b393ddb956d6098af2b57e0ebbb3fb87371
s175295 <s175295@stud.hioa.no>
2017-09-27 10:08:44 +0200

    DB connection almost working

---
+++ WebAppsOppgave1/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppsOppgave1.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string FromDestination { get; set; }
+        public string ToDestination { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
---
+++ WebAppsOppgave1/Models/DB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppsOppgave1.Models
+{
+    public class DB : DbContext
+    {
+        public DB()
+            : base("name=Booking")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<Booking> Booking { get; set; }
+    }
+}
