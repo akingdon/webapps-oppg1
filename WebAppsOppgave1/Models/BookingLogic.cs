3e33b347a48fa842d918149ad070e04b7c8106e5
s175295 <s175295@stud.hioa.no>
2017-10-04 10:31:47 +0200

    Back end validation is now working

---
+++ WebAppsOppgave1/Models/BookingLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppsOppgave1.Models
+{
+    public class BookingLogic
+    {
+        private Booking Booking { get; set; }
+
+        public Boolean DestinationsAreSet(Booking booking)
+        {
+            return !string.IsNullOrWhiteSpace(booking.FromDestination) && !string.IsNullOrWhiteSpace(booking.ToDestination);
+        }
+
+        public Boolean DestinationsAreDifferent(Booking booking)
+        {
+            return !booking.FromDestination.Equals(booking.ToDestination);
+        }
+
+        public Boolean DepartureDateIsBeforeReturnDate(Booking booking)
+        {
+            return booking.FromDate < booking.ToDate;
+        }
+
+        public Boolean DepartureDateIsBeforeToday(Booking booking)
+        {
+            return booking.FromDate < DateTime.Today;
+        }
+    }
+}
