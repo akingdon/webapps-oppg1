72c1e3c7c68c4cd3c7f0ec01fe4e4dd326608ed2
paalet <andreassen.pal@gmail.com>
2017-10-18 11:32:54 +0200

    Added class libariary projects for layers

---
+++ ClassLibrary2/DB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAppsOppgave1.Model;
+
+namespace WebAppsOppgave1.DAL
+{
+    public class DB : DbContext
+    {
+        public DB()
+            : base("name=Booking")
+        {
+            Database.CreateIfNotExists();
+        }
+
+        public DbSet<Booking> Booking { get; set; }
+        public DbSet<Flight> Flight { get; set; }
+        public DbSet<Airport> Airport { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<PostSted> Poststed { get; set; }
+    }
+}
---
+++ ClassLibrary3/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppsOppgave1.Model
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public virtual Airport FromAirport { get; set; }
+        public virtual Airport ToAirport { get; set; }
+        public DateTime Departure { get; set; }
+        public DateTime Arrival { get; set; }
+        public virtual Booking Booking { get; set; }
+        public int Price { get; set; }
+    }
+}
