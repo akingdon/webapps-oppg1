f2a9ee3afbf4e0639051b2bd7ffc3004dda200cc
s175295 <s175295@stud.hioa.no>
2017-10-19 10:47:29 +0200

    Logging errors and events working

--- ClassLibrary2/AdminDAL.cs
+++ ClassLibrary2/AdminDAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,10 @@ namespace WebAppsOppgave1.DAL
 {
     public class AdminDAL
     {
+        private readonly string LogPath = AppDomain.CurrentDomain.BaseDirectory + "\\logs\\";
+        private const string LOG_EVENTS = "log_events.txt";
+        private const string LOG_ERRORS = "log_errors.txt";
+
         public AdminUser AdminInDb(string UserName, byte[] HashedPassword)
         {
             DB Db = new DB();
@@ -52,10 +57,15 @@ namespace WebAppsOppgave1.DAL
 
                 Db.Airport.Add(airport);
                 Db.SaveChanges();
+                int id = airport.Id;
+
+                WriteLogEvent("Added new airport: " + name + " with id: " + id);
+
                 return "ok";
             }
             catch (Exception e)
             {
+                WriteLogError("Adding new airport failed: " + e.Message);
                 return "Adding to DB failed";
             }
         }
@@ -65,11 +75,19 @@ namespace WebAppsOppgave1.DAL
             try
             {
                 Airport airportToEdit = Db.Airport.Find(id);
+
+                string OldName = airportToEdit.Name;
+
                 airportToEdit.Name = name;
                 Db.SaveChanges();
+
+                WriteLogEvent("Edited airport id: " + airportToEdit.Id + ". Name changed from " + OldName + " to " + name);
+
                 return "ok";
             }
-            catch {
+            catch (Exception e)
+            {
+                WriteLogError("Could not edit airport: " + e.Message);
                 return "Editing in DB failed";
             }
         }
@@ -81,10 +99,14 @@ namespace WebAppsOppgave1.DAL
                 Airport airportToDelete = Db.Airport.Find(id);
                 Db.Airport.Remove(airportToDelete);
                 Db.SaveChanges();
+
+                WriteLogEvent("Deleted airport id: " + airportToDelete.Id + " name: " + airportToDelete.Name);
+
                 return "ok";
             }
-            catch
+            catch (Exception e)
             {
+                WriteLogError("Could not delete airport: " + e.Message);
                 return "Deleting from DB failed";
             }
         }
@@ -102,6 +124,60 @@ namespace WebAppsOppgave1.DAL
                 Price = f.Price
             }).ToList();
             return allFlights;
+        }
+
+        private void WriteLogError(string msg)
+        {
+            if (!Directory.Exists(LogPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(LogPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Could not create log folder: " + e.Message);
+                }
+            }
+
+            using (StreamWriter w = File.AppendText(LogPath + LOG_ERRORS))
+            {
+                try
+                {
+                    Logger.Write(msg, w);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Could not write to error log. Exception: " + e.Message);
+                }
+            }
+        }
+
+        private void WriteLogEvent(string msg)
+        {
+            if (!Directory.Exists(LogPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(LogPath);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Could not create log folder: " + e.Message);
+                }
+            }
+
+            using (StreamWriter w = File.AppendText(LogPath + LOG_EVENTS))
+            {
+                try
+                {
+                    Logger.Write(msg, w);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Could not write to database log. Exception: " + e.Message);
+                }
+            }
         }
     }
 }
